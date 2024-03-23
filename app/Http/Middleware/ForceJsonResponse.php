ef43e1f14d5eb7f01ae284957e5399c67e1fe417
weslleymart16 <weslleymartins15@gmail.com>
2024-03-22 21:56:41 -0300

    resolve problema com retorno erro de validação.

---
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->headers->has('Accept')) {
+            $request->headers->set('Accept', 'application/json');
+        }
+
+        return $next($request);
+    }
+}
