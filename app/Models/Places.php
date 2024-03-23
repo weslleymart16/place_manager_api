0ed6f4e343ef78405b6169bcd528ac450fcea6a6
weslleymart16 <weslleymartins15@gmail.com>
2024-03-22 21:14:17 -0300

    cria tabela places

---
+++ app/Models/Places.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Places extends Model
+{
+    use HasFactory;
+
+    protected $table = "places";
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'city',
+        'state',
+    ];
+}
